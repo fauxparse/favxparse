661dcf6a819dc235e23dbe3e8d558391fd295c11
Matt Powell <fauxparse@gmail.com>
2017-05-04 11:07:06 +1200

    fix load problem when not running as server

--- favxparse.rb
+++ favxparse.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
 
 require 'bundler/setup'
-Bundler.require
-
 require 'dotenv/load'
 require 'active_support'
 require 'active_support/dependencies'
 require 'active_support/core_ext/string'
+require 'pry'
+require 'redis'
+require 'twitter'
 
 ActiveSupport::Dependencies.autoload_paths << '.'
 
